f4ff58dbe73b5d2774a65eb41a2951384a85bae9
Matt Hand <74497505+HandMatt@users.noreply.github.com>
2025-07-10 17:07:49 +0100

    test commit hook

--- games/space-defenders/scripts/board-objects/enemies/enemy3.js
+++ games/space-defenders/scripts/board-objects/enemies/enemy3.js
@@ -1,12 +1,13 @@
-var game = this.game || (this.game = {});
+var game = this.game || (this.game={});
 var createjs = createjs || {};
 var lib = lib || {};
 
-(function (game, cjs, lib) {
-  function Enemy3() {
+;(function(game, cjs, lib){
+
+  function Enemy3(){
     game.Enemy.call(this);
 
-    this.addChild(new lib.Enemy3());
+    this.addChild(new lib.Enemy3);
 
     this.originalSpeed = 3.0;
     this.deceleration = 0.02;
@@ -18,4 +19,5 @@ var lib = lib || {};
   Enemy3.prototype = Object.create(game.Enemy.prototype);
 
   game.Enemy3 = Enemy3;
+
 }).call(this, game, createjs, lib);
