a58c2198db24b44d20866d4da073090f81b8dc10
Matt Hand <74497505+HandMatt@users.noreply.github.com>
2025-07-11 13:18:47 +0100

    shoots the balls

--- games/hoop-shoot/scripts/game.js
+++ games/hoop-shoot/scripts/game.js
@@ -16,6 +16,36 @@ var images = images || {};
 
     game.physics.createWorld();
     game.physics.showDebugDraw();
+
+    game.physics.createLevel();
+
+    isPlaying = true;
+
+    game.tickWhenDown = 0;
+    game.tickWhenUp = 0;
+
+    game.stage.on("stagemousedown", function (e) {
+      if (!isPlaying) {
+        return;
+      }
+      game.tickWhenDown = cjs.Ticker.getTicks();
+    });
+
+    game.stage.on("stagemouseup", function (e) {
+      if (!isPlaying) {
+        return;
+      }
+      game.tickWhenUp = cjs.Ticker.getTicks();
+      ticksDiff = game.tickWhenUp - game.tickWhenDown;
+
+      game.physics.shootBall(e.stageX, e.stageY, ticksDiff);
+
+      setTimeout(game.spawnBall, 500);
+    });
+  };
+
+  game.spawnBall = function () {
+    game.physics.spawnBall();
   };
 
   game.tick = function () {
