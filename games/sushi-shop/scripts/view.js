d69fa192b7976abe292c19d776c4be6265904623
Matt Hand <74497505+HandMatt@users.noreply.github.com>
2025-07-15 22:28:38 +0100

    decorates the sushi stand

--- games/sushi-shop/scripts/view.js
+++ games/sushi-shop/scripts/view.js
@@ -17,10 +17,41 @@ var createjs = createjs || {};
   function initResizeHandler() {
     var customerView = document.getElementById("customer-view");
 
-    var resizeCanvas = function () {
-      game.canvas.width = customerView.offsetWidth;
-      game.canvas.height = customerView.offsetHeight;
+    var getBorderWidths = function (element) {
+      // get computed style.
+      var style = getComputedStyle(element);
+      var borderWidths = style.borderWidth.split(" ");
+
+      var top, right, bottom, left;
+      top = right = bottom = left = parseInt(borderWidth[0]);
+
+      if (borderWidths.length >= 2) {
+        right = left = parseInt(borderWidths[1]);
+      }
+      if (borderWidths.length >= 3) {
+        bottom = parseInt(borderWidths[2]);
+      }
+      if (borderWidths.length >= 4) {
+        left = parseInt(borderWidths[3]);
+      }
+
+      // return the 4 values as object.
+      return {
+        top: top,
+        right: right,
+        bottom: bottom,
+        left: left,
+      };
     };
+
+    var resizeCanvas = function () {
+      var borderWidths = getBorderWidths(customerView);
+      game.canvas.width =
+        customerView.offsetWidth - borderWidths.left - borderWidths.right; // border-width of left + right
+      game.canvas.height =
+        customerView.offsetHeight - borderWidths.top - borderWidths.bottom; // border-width of top + bottom
+    };
+
     resizeCanvas();
     repositionCustomer();
 
